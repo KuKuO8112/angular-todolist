2d05acb43efa4e8c3459720fab9b13e08e9495c8
KuKuO <a127007966@gmail.com>
2021-12-02 09:59:09 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+
+  allItems = [
+    {description : "Hit the gym", done : false},
+    {description : "Pay bills", done : true},
+    {description : "Meet George", done : false},
+    {description : "Buy eggs", done : false},
+    {description : "Read a book", done : false},
+    {description : "Organize office", done : false}
+  ];
+
+  get items(){
+    return this.allItems;
+  }
+
+  addItem(description: string) {
+    if(description == "")
+      alert("You must write something!");
+    else{
+      this.allItems.push({
+        description,
+        done: false
+      });
+    }
+  }
+  ifChecked(done: boolean){
+    if(done == true)
+      return "checked"
+    else
+      return ""
+  }
+  changeCheck(item: any){
+    item.done = !item.done;
+  }
+  remove(index: number){
+    this.allItems.splice(index, 1);
+  }
 }
+
